ccb256a450d3f4ad47fa02f48181ca77f977f2e5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-11 17:01:42 -0700

    My first swing, photo quiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,23 +22,27 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+String picture = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQMAAADCCAMAAAB6zFdcAAAAA1BMVEUjsU1owme3AAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIC3AcUIAAFkqh/QAAAAAElFTkSuQmCC";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component component;
 		// 3. use the "createImage()" method below to initialize your Component
-
+component = createImage(picture);
 		// 4. add the image to the quiz window
-
+quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String trueFalse = JOptionPane.showInputDialog("What color is the image");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(trueFalse.equals("green")) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	JOptionPane.showMessageDialog(null, "INCORRECT");
+}
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+quizWindow.remove(component);
 		// 10. find another image and create it (might take more than one line of code)
 
 		// 11. add the second image to the quiz window
